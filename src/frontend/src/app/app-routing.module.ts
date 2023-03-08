55a629bce8d4c6e5ca98c9129f51ec6b6017331d
Marcos Arza garcia <marza-ga@car9s2.42barcelona.com>
2023-03-08 19:20:45 +0100

    empezando con header, footer i home de angular

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo: "users"},
+  {path: "", component: HomeComponent},
   {path: "users", component: UserComponent}
 ];
 
