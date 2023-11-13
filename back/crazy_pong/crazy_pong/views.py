cea6c7e411c2101b383edbcc893f0a54f31871a9
Joan Areste Gonzalez <jareste-@cbr3s1.42barcelona.com>
2023-11-13 02:46:29 +0100

    SPA WORKING

--- back/crazy_pong/crazy_pong/views.py
+++ back/crazy_pong/crazy_pong/views.py
@@ -5,7 +5,14 @@ def hello_view(request):
     data = {'message': 'Hello,hhhhhhh World!'}
     return JsonResponse(data)
 
-def get_hello_message(request):
+def get_home(request):
     data = {'message': '<strong>Hello,holadsfa World!</strong>'}
     return JsonResponse(data)
 
+def get_login(request):
+    data = {
+        'title': 'Login Page',
+        'content': '<strong>Hello, login</strong>',
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
