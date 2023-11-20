41fb2c2e32f02b25b34300d59a8dd034b6c8c9bc
Joan Areste Gonzalez <jareste-@car13s3.42barcelona.com>
2023-11-20 20:30:25 +0100

    doing some tests to send information from front to backend

--- back/crazy_pong/accounts/views.py
+++ back/crazy_pong/accounts/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+
+def get_login_form_page(request):
+    context = {
+        'variable1': 'template variable 1',
+        'variable2': 'template variable 2',
+    }
+    content_html = render_to_string('login/normal_login.html', context)
+    data = {
+        'title': 'Select Logging Mode',
+        'content': content_html,
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
