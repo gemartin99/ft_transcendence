2ca6a0dd84a16cb7093fc3c35b5fccbc8aaca4a1
baltes <baltes-g@student.42barcelona.com>
2023-11-24 12:11:17 +0100

    django send too slow

--- back/crazy_pong/game/consumers.py
+++ back/crazy_pong/game/consumers.py
@@ -1,8 +1,9 @@
 import json
 import random
 import string
+import time
 
-from .match import MatchInfo, PlayerManager, GameManager
+from .match import PlayerManager, GameManager
 from .match_manager import MatchManager
 
 from asgiref.sync import async_to_sync
@@ -11,10 +12,10 @@ from channels.generic.websocket import WebsocketConsumer
 class gameConnection(WebsocketConsumer):
     
     def __init__(self, *args, **kwargs):
-        self.game_ctrl = GameManager()
         self.thread = None
-
+        self.game_ctrl = None
         super().__init__(*args, **kwargs)
+        self.time = time.time()
 
     def connect(self):
         print(self.scope['query_string'])
@@ -30,21 +31,23 @@ class gameConnection(WebsocketConsumer):
 
         if self.game not in MatchManager.threads:
             MatchManager.add_game(self.game, self)
+            self.game_ctrl = GameManager(MatchManager.matches[self.game])
 
         self.thread = MatchManager.threads[self.game]
 
         async_to_sync(self.channel_layer.group_add)(self.game, self.channel_name)
 
         if not self.thread["paddle_one"]:
-            self.paddle_controller = PlayerManager("player1")
+            self.paddle_controller = PlayerManager("player1", MatchManager.matches[self.game])
             self.thread["paddle_one"] = True
+
             if (self.mode == 'IA'):
                 self.game_ctrl.setIA()
                 self.thread['active'] = True
                 self.thread['paddle_two'] = True
 
         elif not self.thread["paddle_two"]:
-            self.paddle_controller = PlayerManager("player2")
+            self.paddle_controller = PlayerManager("player2", MatchManager.matches[self.game])
             self.thread["paddle_two"] = True
 
         if self.thread["paddle_one"] and self.thread["paddle_two"]:
@@ -71,18 +74,24 @@ class gameConnection(WebsocketConsumer):
         else:
             self.send("Unknown command")
 
-    def propagate_state(self):
+    def propagate_state(self, state):
         while True:
             if self.thread:
                 if self.thread["active"]:
+                    self.time = time.time()
                     self.game_ctrl.updateGame()
-
+                    print("Update game" + str(time.time() - self.time))
+                    self.time = time.time()
                     async_to_sync(self.channel_layer.group_send)(
                         self.game,
-                        {"type": "stream_state", "state": MatchInfo.state,},
+                        {"type": "stream_state", "state": state},
                     )
+                    print("Send msg" + str(time.time() - self.time))
+            #time.sleep(1/100)
 
     def stream_state(self, event):
+        time2 = time.time()
         state = event["state"]
-
+        
         self.send(text_data=json.dumps(state))
+        print("Send one msg" + str(time.time() - time2))
