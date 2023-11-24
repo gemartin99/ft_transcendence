37a53e8ca74d1abead7e6aa6ae417b1484b7b129
baltes <baltes-g@student.42barcelona.com>
2023-11-24 12:11:17 +0100

    django send too slow

--- back/crazy_pong/game/match.py
+++ back/crazy_pong/game/match.py
@@ -2,54 +2,10 @@ import random
 import time
 import math
 
-class MatchInfo():
-    state = {
-        "cmd": "update",
-        "idMatch": 0,
-        "type": 1,
-        "player1": {
-            "id": 'nouse',
-            "name": 'nouse',
-            "input": 0,
-        },
-        "player2": {
-            "id": 'nouse2',
-            "name": 'nouse2',
-            "input": 0,
-        },
-        "ball": {
-            "x": 0,
-            "y": 0,
-            "vx": 5,
-            "vy": 3,
-            "radius": 20,
-        },
-        "paddle1": {
-            "x": 30,
-            "y": 300,
-            "width": 20,
-            "height": 150,
-            "vy": 2,
-        },
-        "paddle2": {
-            "x": 1150,
-            "y": 300,
-            "width": 20,
-            "height": 150,
-            "vy": 2,
-        },
-        "score1": 0,
-        "score2": 0,
-        "speed": 6,
-        "isPaused": True,
-        "isGameOver": False,
-        "winner": 0,
-    }
-
 class PlayerManager():
-    def __init__(self, player):
+    def __init__(self, player, state):
 
-        self.state = MatchInfo.state
+        self.state = state
         self.player = self.state[player]
 
     def move(self, direction):
@@ -68,8 +24,8 @@ class GameManager():
     p1 = None
     p2 = None
 
-    def __init__(self):
-        self.state = MatchInfo.state
+    def __init__(self, state):
+        self.state = state
         self.ball = self.state["ball"]
         self.paddle_one = self.state["paddle1"]
         self.paddle_two = self.state["paddle2"]
@@ -83,7 +39,7 @@ class GameManager():
         self.time = time.time()
 
     def updateGame(self):
-        if time.time() - self.time > 1/60:
+        #if time.time() - self.time > 1/60:
             ball = self.ball
             paddle1 = self.paddle_one
             paddle2 = self.paddle_two
@@ -158,6 +114,6 @@ class GameManager():
         self.IA = True
 
     def segfaultThink(self):
-        if (self.ball['y'] > self.paddle_two['y']):
+        if (self.ball['y'] > self.paddle_two['y'] + self.paddle_two['height'] /2 ):
             return 1
         return -1
--- back/crazy_pong/game/match_manager.py
+++ back/crazy_pong/game/match_manager.py
@@ -1,16 +1,60 @@
 import threading
-
+from .match import GameManager
 
 class MatchManager:
 
     threads = {}
-
+    matches = {}
     @classmethod
     def add_game(cls, game_name, consumer_instance):
+        cls.matches[game_name] = {
+            "cmd": "update",
+            "idMatch": 0,
+            "type": 1,
+            "player1": {
+                "id": 'nouse',
+                "name": 'nouse',
+                "input": 0,
+            },
+            "player2": {
+                "id": 'nouse2',
+                "name": 'nouse2',
+                "input": 0,
+            },
+            "ball": {
+                "x": 0,
+                "y": 0,
+                "vx": 5,
+                "vy": 3,
+                "radius": 20,
+            },
+            "paddle1": {
+                "x": 30,
+                "y": 300,
+                "width": 20,
+                "height": 150,
+                "vy": 2,
+            },
+            "paddle2": {
+                "x": 1150,
+                "y": 300,
+                "width": 20,
+                "height": 150,
+                "vy": 2,
+            },
+            "score1": 0,
+            "score2": 0,
+            "speed": 6,
+            "isPaused": True,
+            "isGameOver": False,
+            "winner": 0,
+        }
         cls.threads[game_name] = {
-            "thread": threading.Thread(target=consumer_instance.propagate_state),
+            "thread": threading.Thread(target=consumer_instance.propagate_state, args=(cls.matches[game_name],)),
             "paddle_one": False,
             "paddle_two": False,
+            "player_one": None,
+            "player_two": None,
             "active": False,
         }
         thread = cls.threads[game_name]["thread"]
