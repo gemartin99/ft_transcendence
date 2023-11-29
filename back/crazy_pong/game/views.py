a2958c0818681e8cfd7c13fb7cee22e6716b76df
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-11-29 22:40:16 +0100

    test push to main

--- back/crazy_pong/game/views.py
+++ back/crazy_pong/game/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.shortcuts import render
 
 # Create your views here.
+def get_game_page(request):
+    context = {
+        'variable1': 'template variable 1',
+        'variable2': 'template variable 2',
+    }
+    content_html = render_to_string('game/game.html', context)
+    data = {
+        'title': 'Select Logging Mode',
+        'content': content_html,
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
