98532326ae949d96964f159902f59b7e6202e278
baltes <baltes-g@student.42barcelona.com>
2023-11-30 15:50:41 +0100

    merge completed

--- back/crazy_pong/crazy_pong/urls.py
+++ back/crazy_pong/crazy_pong/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include('accounts.urls')),
-    path('game/', include('game.urls')),,
+    path('game/', include('game.urls')),
+    path('api/', include('api.urls')),
     path('tournament/', include('tournament.urls'))
 ]
