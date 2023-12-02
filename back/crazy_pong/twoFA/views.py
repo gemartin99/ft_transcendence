1b5d1be88e9bd969b0e0384ebea66cd09e28ee69
Joan Areste Gonzalez <jareste-@car7s4.42barcelona.com>
2023-12-02 17:48:50 +0100

    nikelao

--- back/crazy_pong/twoFA/views.py
+++ back/crazy_pong/twoFA/views.py
@@ -19,3 +19,24 @@ def mail(request):
         return JsonResponse({'message': 'messageSent'})
     except Exception as e:
         return JsonResponse({'message': f'Error: {str(e)}'})
+
+
+##NO VA
+from django.core.mail import send_mail
+
+def send_sms_via_email(to, body):
+    # Replace '@txt.att.net' with the actual gateway address for the recipient's carrier (e.g., '@vtext.com' for Verizon)
+    email_address = f'{to}@messaging.sprintpcs.com'
+    print(email_address)
+    subject = ''  # The subject is often ignored in SMS messages
+
+    send_mail(subject, body, 'crazypongreal@hotmail.com', [email_address], fail_silently=False)
+
+def send_sms_view(request):
+    to_phone_number = '+34654508192'  # Replace with the recipient's phone number, without the plus sign and with the country code
+    message_body = 'gestioname el front'
+
+    send_sms_via_email(to_phone_number, message_body)
+    print('hola')
+    return JsonResponse({'message': 'messageSent'})
+##NO VA
