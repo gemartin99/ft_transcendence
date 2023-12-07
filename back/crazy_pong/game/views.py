dac7955bc560e5ec11386bdc89332aeec87e766d
gemartin99 <gerardmg19953@gmail.com>
2023-12-07 20:29:41 +0100

    add button game play vs ia

--- back/crazy_pong/game/views.py
+++ back/crazy_pong/game/views.py
@@ -15,3 +15,16 @@ def get_game_page(request):
         'additionalInfo': 'Some additional information here',
     }
     return JsonResponse(data)
+
+def get_play_page(request):
+    context = {
+        'variable1': 'template variable 1',
+        'variable2': 'template variable 2',
+    }
+    content_html = render_to_string('game/play.html', context)
+    data = {
+        'title': 'Select Logging Mode',
+        'content': content_html,
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
