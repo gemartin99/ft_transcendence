d81fa21b79c29f4a778ef2bda199e755f01a8747
Marcos Arza garcia <marza-ga@car12s4.42barcelona.com>
2023-12-08 21:37:27 +0100

    test commit

--- back/crazy_pong/accounts/urls.py
+++ back/crazy_pong/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import get_home_page, get_login_page, get_login_form_page, get_login42_form_page, change_view, get_register_new_account_page, create_account, do_login
+from .views import get_home_page, get_login_page, get_login_form_page, get_login42_form_page, change_view, get_register_new_account_page, create_account, do_login, get_profile_page
 
 urlpatterns = [
      path('', change_view, name='change_view'),
@@ -10,7 +10,8 @@ urlpatterns = [
      path('login/identify/', get_login_form_page, name='get_login_form_page'),
      path('login/42identify/', get_login42_form_page, name='get_login42_form_page'),
      path('register/', get_register_new_account_page, name='get_register_new_account_page'),
-     path('register/new/', create_account, name='create_account'),
+     path('register/', get_register_new_account_page, name='get_register_new_account_page'),
+     path('profile/', get_profile_page, name='get_profile_page'),
 ]
 
 
