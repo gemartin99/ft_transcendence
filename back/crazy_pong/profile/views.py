72969f59c00da8342ef7a4a1ad9182d9d4b01c76
Marcos Arza garcia <marza-ga@car3s1.42barcelona.com>
2023-12-10 22:42:44 +0100

    todavia me falta

--- back/crazy_pong/profile/views.py
+++ back/crazy_pong/profile/views.py
@@ -32,3 +32,37 @@ def get_profile_page(request):
         'additionalInfo': 'Some additional information here',
     }
     return JsonResponse(data)
+
+def get_edit_profile_page(request):
+    user = Authentification.get_auth_user(request)
+    if not user:
+        return JsonResponse({'redirect': '/users/login/'})
+    context = {
+        'variable1': 'template variable 1',
+        'variable2': 'template variable 2',
+        'user': user,
+    }
+    content_html = render_to_string('profile/edit-profile.html', context)
+    data = {
+        'title': 'Select Logging Mode',
+        'content': content_html,
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
+
+def get_twofactor_profile_page(request):
+    user = Authentification.get_auth_user(request)
+    if not user:
+        return JsonResponse({'redirect': '/users/login/'})
+    context = {
+        'variable1': 'template variable 1',
+        'variable2': 'template variable 2',
+        'user': user,
+    }
+    content_html = render_to_string('profile/edit-twofactor.html', context)
+    data = {
+        'title': 'Select Logging Mode',
+        'content': content_html,
+        'additionalInfo': 'Some additional information here',
+    }
+    return JsonResponse(data)
--- back/crazy_pong/twoFA/urls.py
+++ back/crazy_pong/twoFA/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
 	# path('', get_home_page, name='get_home_page'),
     # # path('request1/', request_login, name='request_login'),
     # path('register/', create_account, name='create_account'),
-    # path('mail/', mail, name='mail'),
-
+    # path('mail/', mail, name='mail'), 
     path('enable_google2FA', activateGoogle2FA, name='activateGoogle2FA'),    
     path('enable_mail2FA', activateMail2FA, name='activateMail2FA'),
     # path('sms/', send_sms_view, name='send_sms_view'),
