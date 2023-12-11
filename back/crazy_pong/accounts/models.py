efae3e1aa244028534472bebb25e1513a6e8c62d
Joan Areste Gonzalez <jareste-@car3s3.42barcelona.com>
2023-12-11 16:39:56 +0100

    anadido .env y metido .env en gitignore

--- back/crazy_pong/accounts/models.py
+++ back/crazy_pong/accounts/models.py
@@ -14,7 +14,7 @@ class Usermine(models.Model):
     totp = models.CharField(max_length=255, default='')
     mail2FA = models.BooleanField(default=False)
     google2FA = models.BooleanField(default=False)
-    mail2FACode = models.IntegerField(max_length=6, default=-1)
+    mail2FACode = models.CharField(max_length=6, default=-1)
 
     class Meta:
         abstract = False
