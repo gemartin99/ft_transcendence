d7f932ca59e22f39a6860b7d7a50324e22e90a0b
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-12-12 15:59:18 +0100

    login 42 already working (need to proceed the data on the backend and create user)

--- back/crazy_pong/FT_OAuth/urls.py
+++ back/crazy_pong/FT_OAuth/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import check_42
 # from .views import 
 
 app_name = 'FT_OAuth'
 urlpatterns = [
+    path('42/', check_42, name='check_42'),
 
 
 ]
