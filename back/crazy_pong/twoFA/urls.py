d8217dd358c876f1fe12835a01b4fedee3a7a5d6
Marcos Arza garcia <marza-ga@car3s1.42barcelona.com>
2023-12-17 18:52:34 +0100

    google set two factor

--- back/crazy_pong/twoFA/urls.py
+++ back/crazy_pong/twoFA/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import activateGoogle2FA, activateMail2FA, verifyMailCode, disableTwoFactor, get_set_mail2FA_page
+from .views import activateGoogle2FA, activateMail2FA, verifyMailCode, disableTwoFactor, get_set_mail2FA_page, get_set_google2FA_page, verify_totp, enable_totp
 
 app_name = 'twoFA'
 urlpatterns = [
@@ -10,12 +10,11 @@ urlpatterns = [
     # path('mail/', mail, name='mail'),
 
     path('set-mail2FA/', get_set_mail2FA_page, name='get_set_mail2FA_page'),
+    path('set-google2FA/', get_set_google2FA_page, name='get_set_google2FA_page'),
     path('google2FA/', activateGoogle2FA, name='activateGoogle2FA'),    
     path('mail2FA/', activateMail2FA, name='activateMail2FA'),
     path('verifyMailCode/', verifyMailCode, name='verifyMailCode'),
+    path('verifyGoogleCode/', verify_totp, name='verify_totp'),
     path('disable/', disableTwoFactor, name='disableTwoFactor'),
-    # path('sms/', send_sms_view, name='send_sms_view'),
-    # path('getQR/', enable_totp, name='enable_totp'),
-    # path('checkQR/', verify_totp, name='verify_totp'),
-
+    path('getQR/', enable_totp, name='enable_totp'),
 ]
