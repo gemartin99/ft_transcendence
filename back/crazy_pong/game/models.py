8ab89109237177548e8fb9982b0808f31e3fd517
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-12-21 11:56:26 +0100

    trying to print match history

--- back/crazy_pong/game/models.py
+++ back/crazy_pong/game/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-from accounts.models import Usermine
 # Create your models here.
-
 
 class Match(models.Model):
     match_id = models.CharField(primary_key = True, max_length=100)
-    player1 = models.ForeignKey(Usermine, on_delete=models.CASCADE, related_name='player1')
-    player2 = models.ForeignKey(Usermine, on_delete=models.CASCADE, related_name='player2')
+    player1 = models.CharField(max_length=100, default='')
+    player2 = models.CharField(max_length=100, default='')
     player1_score = models.IntegerField()
     player2_score = models.IntegerField()
-    
