7f0f8095b13341f2c2b2cba04d0b51331cd125fb
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-01-14 16:04:46 +0100

    cleaning the code

--- back/crazy_pong/security/security.py
+++ back/crazy_pong/security/security.py
@@ -24,18 +24,11 @@ class Security:
 
     @staticmethod
     def is_valid_username(username):
-        # Define a regular expression for allowed characters in the username
         allowed_characters = re.compile(r'^[a-zA-Z0-9_\-]+$')
-        print('here:', allowed_characters)
-        # Check if the username matches the pattern
         return bool(allowed_characters.match(username))
 
     @staticmethod
     def is_valid_email(email):
-        # Regular expression for a basic email validation
         email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-
-        # Use re.match() to check if the email matches the pattern
         match = re.match(email_regex, email)
-
         return bool(match)
--- front/src/js/profile.js
+++ front/src/js/profile.js
@@ -1,43 +1,5 @@
-// url = "crazy-pong.com"
-// url = "localhost"
 var url = window.location.hostname;
-
-//baseurl = "http://crazy-pong.com"
-// baseurl = "http://localhost";
 var baseurl = window.location.origin;
-
-// function submitForm(e) {
-//     e.preventDefault();
-//     const loginForm = document.querySelector("#updateUserForm");
-//     const formData = new FormData(e.target);
-
-//     // Convert form data to a JSON object
-//     const formDataObject = {};
-//     for (const [key, value] of formData.entries()) {
-//         // If the value is empty, set it to null
-//         formDataObject[key] = value || null;
-//     }
-
-//     console.log('formDataObject:', formDataObject);
-
-//     // You can now use the formDataObject to send the data using fetch or another AJAX method
-//     fetch(baseurl + ':8000/profile/UpdateInfo/', {
-//         method: 'POST',
-//         headers: {'Content-Type': 'application/json'},
-//         credentials: 'include',
-//         body: JSON.stringify(formDataObject),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Response:', data.message);
-//         setFormMessage(loginForm, "error", data.message);
-
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//         setFormMessage(loginForm, "error", "Invalid username/password combination");
-//     });
-// }
 
 function submitForm(e) {
     e.preventDefault();
@@ -50,7 +12,7 @@ function submitForm(e) {
         formData.append('avatar', avatarFile);
     }
 
-    // You can now use the formData object to send the data using fetch or another AJAX method
+
     var lang = getLang()
     fetch(baseurl + ':8000/profile/UpdateInfo/', {
         method: 'POST',
@@ -62,6 +24,7 @@ function submitForm(e) {
     })
     .then(response => response.json())
     .then(data => {
+        console.log("jajajaja",data);
         if (data.redirect) {
             handleRedirect(data.redirect);
             return ;
