193888abee0a377571ba814366f8f55ed1adad01
baltes <baltes-g@student.42barcelona.com>
2024-01-23 10:22:44 +0100

    wss to ws

--- back/crazy_pong/accounts/routing.py
+++ back/crazy_pong/accounts/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/login/$', consumers.loginConnection.as_asgi()),
+    re_path(r'ws/login/$', consumers.loginConnection.as_asgi()),
 ]
