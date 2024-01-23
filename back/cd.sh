a3ddd1addee51aec079b7b640a96f09f750bc096
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-01-23 17:57:57 +0100

    mama que hi ha avui per sopar? per sopar? un parell de gitadds

--- back/cd.sh
+++ back/cd.sh
@@ -2,4 +2,4 @@
 
 cd /app/crazy_pong
 
-daphne -e ssl:443:privateKey=crazy-pong.com_private_key.key:certKey=crazy-pong.com_ssl_certificate.cer crazy_pong.asgi:application
+daphne -e ssl:443:privateKey=/etc/ssl/private/cert.key:certKey=/etc/ssl/certs/cert.crt crazy_pong.asgi:application
