4002f33beb0e45f093fcba59e9e25b0f4ef4bdd6
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-01-23 17:40:43 +0100

    💿

--- back/crazy_pong/crazy_pong/asgi.py
+++ back/crazy_pong/crazy_pong/asgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
-import crazy_pong.accounts.routing
-import crazy_pong.game.routing
+import accounts.routing
+import game.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
